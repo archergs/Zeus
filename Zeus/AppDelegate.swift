cdd8f1624a2d903e9055cb1b7bd8372b331e6ea7
Archer Gardiner-Sheridan <archergardinersheridan@gmail.com>
2019-10-29 17:57:18 +1030

    Initial Commit

---
+++ Zeus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Zeus
+//
+//  Created by Archer Gardiner-Sheridan on 29/10/19.
+//  Copyright © 2019 Archer Gardiner-Sheridan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Zeus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Zeus
+//
+//  Created by Archer Gardiner-Sheridan on 29/10/19.
+//  Copyright © 2019 Archer Gardiner-Sheridan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
